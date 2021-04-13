df64cd1fda6e506d295b1d084bb7cead0d2890d1
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-04-13 16:58:46 +0200

    I added the cookie-session library and the login panel works

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', (req, res,next) =>
+{
+  if (!req.session.admin)
+  {
+    res.redirect('/login');
+    return;
+  }
+
+  next();
+});
 /* GET home page. */
 router.get('/', (req, res, next)=> {
   res.render('admin', { title: 'Admin' });
